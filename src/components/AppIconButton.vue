c7e65a37f7621da488bd830808c10964d680b0fe
Katcha <kratochvilova93@email.cz>
2022-10-31 22:21:03 +0100

    Created AppIconButton and AppTooltip comps

---
+++ src/components/AppIconButton.vue
@@ -0,0 +1,50 @@
+<template>
+	<div
+		class="relative flex justify-center items-center bg-white/50 rounded-md cursor-pointer hover:bg-white/70 transition duration-200"
+	>
+		<button
+			:class="`w-${size} h-${size}`"
+			@mouseenter="toggleTooltip()"
+			@mouseleave="toggleTooltip()"
+		>
+			<i :class="`text-white text-xl ${icon}`"></i>
+		</button>
+		<AppTooltip v-if="showTooltip" :msg="tooltip" />
+	</div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import AppTooltip from './AppTooltip.vue'
+defineProps({
+	icon: {
+		type: String,
+		required: true,
+	},
+	size: {
+		type: String,
+		required: true,
+	},
+	tooltip: {
+		type: String,
+	},
+})
+
+const showTooltip = ref(false)
+const toggleTooltip = () => {
+	showTooltip.value = !showTooltip.value
+}
+</script>
+
+<style scoped>
+.tooltip-enter-from,
+.tooltip-leave-to {
+	opacity: 0;
+	transform: scale(0.9);
+}
+
+.tooltip-enter-active,
+.tooltip-leave-active {
+	transition: all 0.25s;
+}
+</style>
---
+++ src/components/AppTooltip.vue
@@ -0,0 +1,16 @@
+<template>
+	<span
+		class="absolute left-7 top-6 z-30 max-h-10 min-w-max bg-gray-200/70 rounded-md px-1 py-0.5 text-sm"
+		v-if="props.msg"
+	>
+		{{ props.msg }}
+	</span>
+</template>
+
+<script setup>
+const props = defineProps({
+	msg: {
+		type: String || undefined,
+	},
+})
+</script>
