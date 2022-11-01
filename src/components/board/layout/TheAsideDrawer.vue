57e94cb7fa51cf937ba25586218eb2ae70030df8
Katcha <kratochvilova93@email.cz>
2022-11-01 19:45:36 +0100

    Added router links into TheAsideDrawer and TheAsideList for navigating tp current project

--- src/components/board/layout/TheAsideDrawer.vue
+++ src/components/board/layout/TheAsideDrawer.vue
@@ -4,55 +4,44 @@
 			class="fa-solid fa-chevron-left text-white/50 hover:text-white transition duration-200 self-end mb-0.5 cursor-pointer"
 			@click="toggleDrawer()"
 		></i>
-		<div
-			class="flex items-center gap-2 shrink-0 hover:bg-white/50 transition duration-300 cursor-pointer py-1 pl-1 rounded-md"
+		<RouterLink
+			v-for="project in projects"
+			:key="project.id"
+			:to="{
+				name: 'project',
+				params: { userId: project.userId, projectId: project.id },
+			}"
 		>
-			<img
-				class="w-9 h-9 rounded"
-				src="../../../../public/img/groot.jpg"
-				alt=""
-			/>
-			<p
-				class="text-lg text-white font-semibold truncate whitespace-nowrap overflow-hidden"
+			<div
+				class="flex items-center gap-2 shrink-0 hover:bg-white/50 transition duration-300 cursor-pointer py-1 pl-1 rounded-md"
 			>
-				Project name
-			</p>
-		</div>
-		<div
-			class="flex items-center gap-2 shrink-0 hover:bg-white/50 transition duration-300 cursor-pointer py-1 pl-1 rounded-md"
-		>
-			<img
-				class="w-9 h-9 rounded"
-				src="../../../../public/img/groot.jpg"
-				alt=""
-			/>
-			<p
-				class="text-lg text-white font-semibold truncate whitespace-nowrap overflow-hidden"
-			>
-				Project name
-			</p>
-		</div>
-		<div
-			class="flex items-center gap-2 shrink-0 hover:bg-white/50 transition duration-300 cursor-pointer py-1 pl-1 rounded-md"
-		>
-			<img
-				class="w-9 h-9 rounded"
-				src="../../../../public/img/groot.jpg"
-				alt=""
-			/>
-			<p
-				class="text-lg text-white font-semibold truncate whitespace-nowrap overflow-hidden"
-			>
-				Project name
-			</p>
-		</div>
+				<img
+					class="w-9 h-9 rounded"
+					:src="`/img/${project.avatar}`"
+					:alt="project.projectName"
+				/>
+				<p
+					class="text-lg text-white font-semibold truncate whitespace-nowrap overflow-hidden"
+				>
+					{{ project.projectName }}
+				</p>
+			</div>
+		</RouterLink>
 	</div>
 </template>
 
 <script setup>
+import { computed } from 'vue'
+import { useProjectStore } from '../../../stores/project'
+
 const emit = defineEmits(['toggleDrawer'])
 
 const toggleDrawer = () => {
 	emit('toggleDrawer')
 }
+
+const projectStore = useProjectStore()
+const projects = computed(() => {
+	return projectStore.projects
+})
 </script>
--- src/components/board/layout/TheAsideList.vue
+++ src/components/board/layout/TheAsideList.vue
@@ -4,28 +4,34 @@
 			class="fa-solid fa-chevron-right text-white/50 hover:text-white transition duration-200 self-end mb-0.5 cursor-pointer"
 			@click="toggleDrawer()"
 		></i>
-
-		<img
-			class="w-9 h-9 rounded"
-			src="../../../../public/img/groot.jpg"
-			alt=""
-		/>
-		<img
-			class="w-9 h-9 rounded"
-			src="../../../../public/img/groot.jpg"
-			alt=""
-		/>
-		<img
-			class="w-9 h-9 rounded"
-			src="../../../../public/img/groot.jpg"
-			alt=""
-		/>
+		<RouterLink
+			:to="{
+				name: 'project',
+				params: { userId: project.userId, projectId: project.id },
+			}"
+			v-for="project in projects"
+			:key="project.id"
+		>
+			<img
+				class="w-9 h-9 rounded"
+				:src="`/img/${project.avatar}`"
+				:alt="project.projectName"
+			/>
+		</RouterLink>
 	</div>
 </template>
 
 <script setup>
+import { computed } from 'vue'
+import { useProjectStore } from '../../../stores/project'
+
 const emit = defineEmits(['toggleDrawer'])
 const toggleDrawer = () => {
 	emit('toggleDrawer')
 }
+
+const projectStore = useProjectStore()
+const projects = computed(() => {
+	return projectStore.projects
+})
 </script>
