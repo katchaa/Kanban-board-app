b5e28d32689e8b63dcc686552055da5e3223b66e
Katcha <kratochvilova93@email.cz>
2023-02-23 17:52:59 +0100

    Fix: Change default domain to localhost

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
+import dns from 'dns'
+dns.setDefaultResultOrder('verbatim')
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
